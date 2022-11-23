60aac96e0e0492e3eb1c5adff3a47ddee4412a46
Bharat Nischal <nischalbharat4819@gmail.com>
2022-11-23 19:55:02 +0530

    Cross communication

--- popup.js
+++ popup.js
@@ -1,6 +1,8 @@
 const p = document.getElementById('hello-world');
 const button = document.getElementById('button');
+
 // chrome.action.disable();
 button.addEventListener('click', () => {
   p.style.color = 'red';
+  chrome.runtime.sendMessage({ message: "hello from action popup" });
 });
