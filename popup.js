ab17e52206c7588cb6a450d733e2a2ef9022998e
Bharat Nischal <nischalbharat4819@gmail.com>
2022-11-23 22:29:59 +0530

    Storage

--- popup.js
+++ popup.js
@@ -1,10 +1,18 @@
 const p = document.getElementById('hello-world');
 const button = document.getElementById('button');
+const countEl = document.getElementById('count');
+let count = 0;
+
+chrome.storage.sync.get(['count'], function(res) {
+  count = res.count || 0;
+  countEl.textContent = count;
+});
+
 
 // chrome.action.disable();
 button.addEventListener('click', () => {
-  p.style.color = 'red';
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    chrome.tabs.sendMessage(tabs[0].id, { message: "hello from action popup" });
+  count++;
+  chrome.storage.sync.set({ count }, function() {
+    countEl.textContent = count;
   });
 });
